8e1060e0085053b012fcfefed6ebe053fce83b53
aldrisheesh <roialdrichsantos.18@gmail.com>
2025-06-22 21:46:54 +0800

    edited things

--- application-system/src/main/java/com/group_9/project/AccountSubsPage.java
+++ application-system/src/main/java/com/group_9/project/AccountSubsPage.java
@@ -212,7 +212,7 @@ public class AccountSubsPage extends Template {
                             "Plan removed from your subscriptions."
                     );
                     new AccountSubsPage().setVisible(true);
-                    AccountSubsPage.this.dispose();
+                    dispose();
                 } else {
                     CustomDialogUtil.showStyledErrorDialog(
                             AccountSubsPage.this,
